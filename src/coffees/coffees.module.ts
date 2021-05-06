11fe941876012eb7b364f1e30a50e70637cd09c7
박지상 <parkjisang@bagjisang-ui-MacBookPro.local>
2021-05-06 17:38:11 +0900

    Value based Providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -6,10 +6,12 @@ import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from '../events/entities/event.entity';
 
+class MockCoffeeServices {}
+
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
-  providers: [CoffeesService],
+  providers: [{ provide: CoffeesService, useValue: new MockCoffeeServices() }],
   exports: [CoffeesService],
 })
 export class CoffeesModule {}
